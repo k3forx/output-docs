87f865e9e2d4b0ad6c42e0e31ff8ad65463a0fa3
Kanata Miyahana <afiri.bupa@gmail.com>
2024-06-20 22:24:29 +0900

    update

--- golang/iterator/main.go
+++ golang/iterator/main.go
@@ -1,24 +1,34 @@
 package main
 
-import (
-	"iter"
-)
+import "fmt"
 
 func main() {
-
+	for v := range g(f) {
+		fmt.Println(v)
+	}
 }
 
-func NoValueIterator(yield func() bool) bool {
-	return yield()
+func f(yield func(int) bool) {
+	for _, num := range []int{1, 0, 2, 3, 4, 5} {
+		if !yield(num) {
+			return
+		}
+	}
 }
 
-func FilterOdd(nums []int) iter.Seq[int] {
-	return func(yield func(int) bool) {
-		for _, n := range nums {
-			if n%2 == 1 {
-				if !yield(n) {
-					return
-				}
+func g(f func(yield func(int) bool)) func(yield func(string) bool) {
+	maps := map[int]string{
+		1: "one", 2: "two", 3: "three",
+		4: "four", 5: "five",
+	}
+	return func(yield func(string) bool) {
+		for v := range f {
+			str, ok := maps[v]
+			if len(str) > 3 {
+				break
+			}
+			if ok && !yield(str) {
+				return
 			}
 		}
 	}
