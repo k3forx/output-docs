377923a0773e78a91f7c0315af34b7b7d974441b
Kanata Miyahana <afiri.bupa@gmail.com>
2024-06-22 12:51:24 +0900

    update go version to 1.23rc1

--- golang/iterator/go.mod
+++ golang/iterator/go.mod
@@ -1,3 +1,3 @@
 module github.com/k3forx/iterator
 
-go 1.22.3
+go 1.23rc1
--- golang/iterator/main.go
+++ golang/iterator/main.go
@@ -3,33 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	for v := range g(f) {
+	for v := range f {
 		fmt.Println(v)
-	}
-}
-
-func f(yield func(int) bool) {
-	for _, num := range []int{1, 0, 2, 3, 4, 5} {
-		if !yield(num) {
+		if v == 3 {
 			return
 		}
 	}
 }
 
-func g(f func(yield func(int) bool)) func(yield func(string) bool) {
-	maps := map[int]string{
-		1: "one", 2: "two", 3: "three",
-		4: "four", 5: "five",
-	}
-	return func(yield func(string) bool) {
-		for v := range f {
-			str, ok := maps[v]
-			if len(str) > 3 {
-				break
-			}
-			if ok && !yield(str) {
-				return
-			}
+func f(yield func(int) bool) {
+	for _, num := range []int{1, 2, 3, 4, 5} {
+		if !yield(num) {
+			return
 		}
 	}
 }
