5cb13fb0034f4160c325faae4758b711eedb615b
Kanata Miyahana <kanata-miyahana@KanatanoMacBook-Pro.local>
2024-09-13 09:31:57 +0900

    add elasticsearch

---
+++ elasticsearch/go.mod
@@ -0,0 +1,5 @@
+module github.com/k3forx/elasticsearch
+
+go 1.23.1
+
+require github.com/elastic/go-elasticsearch/v7 v7.17.10
---
+++ elasticsearch/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/elastic/go-elasticsearch/v7"
+)
+
+func main() {
+	_, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{"http://localhost:9200"},
+	})
+	if err != nil {
+		log.Fatalf("failed to create elasticsearch client: %v", err)
+	}
+}
